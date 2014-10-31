34c8b3fcfa1d5fc3d1d8bd22d3424b096a87a543
tenobu <bizan09@bizankomumakku09-no-MacBook-Air.local>
2014-11-01 00:53:39 +0900

    UIDocument

--- PhotosToInst/ViewController.m
+++ PhotosToInst/ViewController.m
@@ -12,6 +12,8 @@
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
+@property (retain, nonatomic) UIDocumentInteractionController *dic;
+
 @end
 
 @implementation ViewController
@@ -29,14 +31,68 @@
 - (IBAction)button_Camera_Action:(id)sender
 {
 
+	UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+	
+	[picker setDelegate: self];
+	
+	picker.sourceType    = UIImagePickerControllerSourceTypeCamera;
+	picker.allowsEditing = YES;
+	
+	[self presentViewController: picker animated: YES completion: nil];
+	
 }
 
 - (IBAction)button_CameraRoll_Action:(id)sender
 {
 
+	UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+	
+	[picker setDelegate: self];
+	
+	picker.sourceType    = UIImagePickerControllerSourceTypePhotoLibrary;
+	picker.allowsEditing = YES;
+	
+	[self presentViewController: picker animated: YES completion: nil];
+
+}
+
+- (void)shareInstagram
+{
+	
 	UIImage *image = self.imageView.image;
 	
-	NSString *savePath = [NSHomeDirectory() stringByAppendingPathComponent: 
+	NSString *savePath = [NSHomeDirectory() stringByAppendingPathComponent: @"Documents/Test.ig"];
+	
+	[UIImagePNGRepresentation(image) writeToFile: savePath atomically: YES];
+	
+	CGRect rect = CGRectMake( 0, 0, 0, 0 );
+	NSString *jpgPath = [NSHomeDirectory() stringByAppendingPathComponent: @"Documents/Test.ig"];
+	NSURL *igImageHookFile = [[NSURL alloc] initWithString: [NSString stringWithFormat: @"file://%@", jpgPath]];
+							  
+	self.dic.UTI = @"com.instagran.photo";
+	self.dic = [self setupControllerWithURL: igImageHookFile usingDelegate: self];
+	self.dic = [UIDocumentInteractionController interactionControllerWithURL: igImageHookFile];
+	
+	[self.dic presentOpenInMenuFromRect: rect inView: self.view animated: YES];
+	
+	NSURL *instagramURL = [NSURL URLWithString: @"instagram://media?id=MEDIA_ID"];
+	
+	if ( [[UIApplication sharedApplication] canOpenURL: instagramURL] ) {
+		
+		[self.dic presentOpenInMenuFromRect: rect inView:<#(UIView *)#>self.View animated: YES];
+		
+	} else {
+		
+		NSLog( @"No Instagram Found" );
+		
+	}
+	
+}
+
+- (UIDocumentInteractionController *)setupControllerWithURL: (NSURL *)fileURL usingDelegate: (id < UIDocumentInteractionControllerDelegate >)
+{
+
+	
 }
 
 @end
